80ccdc433c3fc2f2afd3f9d39f12a1e961ad0fc9
Zeynep Sude Demir <161715446+zeynn007@users.noreply.github.com>
2024-10-13 12:49:26 +0300

    Add files via upload

---
+++ baslangic.js
@@ -0,0 +1,13 @@
+function cift(a) {
+    for (var i = 1; i <= a; i++) {
+        if (i % 2 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+var sayı = prompt("Bir sayı giriniz:");
+sayı = Number(sayı);
+
+
+
